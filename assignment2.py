fe3291d7d6c82bed56e1b30f9d532740d7305498
Rathodpremdas <97022112+Rathodpremdas@users.noreply.github.com>
2022-01-14 16:12:27 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,16 @@
+
+"Write a pseudo-code to move from the start (S) to the end (E) in the maze."
+while(start != end) :
+
+   if(Is_Next_Block_Bomb) :
+
+      move→Down
+
+      move→Down
+
+    else:
+
+       move→Right
+
+
+
