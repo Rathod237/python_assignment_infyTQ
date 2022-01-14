6d00e2b8de235692e03cab00e3f89a21fe53b693
Rathodpremdas <97022112+Rathodpremdas@users.noreply.github.com>
2022-01-14 16:40:02 +0530

    Add files via upload
    minimum number among 3 numbers

---
+++ min_amog_3_numbers.py
@@ -0,0 +1,22 @@
+
+"Write a pseudo-code to move from the start (S) to the end (E) in the maze."
+
+Number1=input("enter Number1")
+Number2=input("enetr number2")
+Number3=input("number3:")
+
+if(Number1 < Number2):
+   if(Number1 < Number3):
+       print("Num1 is the smallest")
+   else:
+     print("Num3 is the smallest")
+
+elif(Number2 < Number3):
+       print("Num2 is the smallest")
+else:
+     print("Num3 is the smallest")
+
+
+
+
+
